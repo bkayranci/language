2d9676f65337a74e24730c073647deadf0753b60
Türkalp Burak KAYRANCIOĞLU <bkayranci@gmail.com>
2018-04-23 16:01:44 +0300

    trait fix

--- src/LanguageTrait.php
+++ src/LanguageTrait.php
@@ -7,6 +7,6 @@ trait LanguageTrait
 {
     public function languages()
     {
-        return $this->belongsToMany(Language::class, 'language_'.strtolower(class_basename($this)), strtolower(class_basename($this)).'_id', 'language_id');
+        return $this->belongsToMany(Language::class, 'language_'.$this->table, $this->table.'_id', 'language_id');
     }
 }
